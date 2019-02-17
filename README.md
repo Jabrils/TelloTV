0b75baba15967545b78999253b74af6d69a31ff1
Jabrils <ankelzspam@gmail.com>
2019-02-17 01:41:12 -0800

    AI ADDED

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # TelloTV
-# If you're having trouble, make sure that you have installed all the dependencies, pygame, openCV, PYTHON lol, & update the firmware on your Tello Drone via the official app!
+TelloTV is a rather simplistic approach to be able to launch your Tello drone & hav it track your face. This approach has been tested & proven to work with the DJI Tello Drone (Non - Educational Version I believe, but may still work with the educational version?)
+
+Tested with Python 3.6, but it also may be compatabile with other versions.
+
+## Example
+Coming soon...
+
+## Install
+```
+$ pip install requirements.txt
+```
+
+## Usage
+```
+  -h, --help            ** = required
+  -d DISTANCE, --distance DISTANCE
+                        use -d to change the distance of the drone. Range 0-6
+                        (default: 3)
+  -ss, --save_session   add the -ss flag to save your session as an image
+                        sequence in the Sessions folder (default: False)
+  -D, --debug           add the -D flag to enable debug mode. Everything works
+                        the same, but no commands will be sent to the drone
+                        (default: False)
+```
+
+## Roadmap
+- Add a movement gradient dependent on distance from subject
+- Add more to the facial recog to be able to tell when the drone needs to fly left or right
+- Use pose estimation for input commands
+- Add a function where the drone will ignore all faces except the one you specify
+
+# Credits
+This script has been adapted from Damià Fuentes Escoté's [TelloSDKPy](https://github.com/damiafuentes/DJITelloPy) script, please check it out if you want to learn more about that.
+
+#### AI
+- **Jabrils**
+
+#### Backend Thanks
+- **Damià Fuentes Escoté** 
--- TelloTV.py
+++ TelloTV.py
@@ -1,43 +1,52 @@
 from djitellopy import Tello
 import cv2
-import pygame
-from pygame.locals import *
 import numpy as np
 import time
 import datetime
 import os
+import argparse
+
+# standard argparse stuff
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, add_help=False)
+parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS,
+                    help='** = required')
+parser.add_argument('-d', '--distance', type=int, default=3,
+    help='use -d to change the distance of the drone. Range 0-6')
+parser.add_argument('-ss', "--save_session", action='store_true',
+    help='add the -ss flag to save your session as an image sequence in the Sessions folder')
+parser.add_argument('-D', "--debug", action='store_true',
+    help='add the -D flag to enable debug mode. Everything works the same, but no commands will be sent to the drone')
+
+args = parser.parse_args()
 
 # Speed of the drone
-S = 60
+S = 20
+S2 = 5
+UDOffset = 150
+
+# this is just the bound box sizes that openCV spits out *shrug*
+faceSizes = [90, 136, 202, 304, 456, 684, 1026]
+tDistance = args.distance
+
 # Frames per second of the pygame window display
 FPS = 25
 dimensions = (960, 720)
 
-ddir = "Session {}".format(str(datetime.datetime.now()).replace(':','-').replace('.','_'))
-os.mkdir(ddir)
+
+# 
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+if args.save_session:
+    ddir = "Sessions"
+    os.mkdir(ddir)
+
+    ddir = "Sessions/Session {}".format(str(datetime.datetime.now()).replace(':','-').replace('.','_'))
+    os.mkdir(ddir)
 
 class FrontEnd(object):
-    """ Maintains the Tello display and moves it through the keyboard keys.
-        Press escape key to quit.
-        The controls are:
-            - T: Takeoff
-            - L: Land
-            - Arrow keys: Forward, backward, left and right.
-            - A and D: Counter clockwise and clockwise rotations
-            - W and S: Up and down.
-    """
+    
     def __init__(self):
-        # Init pygame
-        pygame.init()
-
-        # Creat pygame window
-        pygame.display.set_caption("Tello video stream")
-        self.screen = pygame.display.set_mode([dimensions[0],dimensions[1]])
-
-        # 
-        pygame.font.init() # you have to call this at the start, 
-                   # if you want to use this module.
-
         # Init Tello object that interacts with the Tello drone
         self.tello = Tello()
 
@@ -49,9 +58,6 @@ class FrontEnd(object):
         self.speed = 10
 
         self.send_rc_control = False
-
-        # create update timer
-        pygame.time.set_timer(USEREVENT + 1, 50)
 
     def run(self):
 
@@ -75,20 +81,14 @@ class FrontEnd(object):
         frame_read = self.tello.get_frame_read()
 
         should_stop = False
-        while not should_stop:
+        imgCount = 0
+        self.tello.get_battery()
 
-            for event in pygame.event.get():
-                if event.type == USEREVENT + 1:
-                    self.update()
-                elif event.type == QUIT:
-                    should_stop = True
-                elif event.type == KEYDOWN:
-                    if event.key == K_ESCAPE:
-                        should_stop = True
-                    else:
-                        self.keydown(event.key)
-                elif event.type == KEYUP:
-                    self.keyup(event.key)
+        if args.debug:
+            print("DEBUG MODE ENABLED!")
+
+        while not should_stop:
+            self.update()
 
             if frame_read.stopped:
                 frame_read.stop()
@@ -96,84 +96,153 @@ class FrontEnd(object):
 
             theTime = str(datetime.datetime.now()).replace(':','-').replace('.','_')
 
-            self.screen.fill([0, 0, 0])
             frame = cv2.cvtColor(frame_read.frame, cv2.COLOR_BGR2RGB)
-            cv2.imwrite("{}/tellocap{}.jpg".format(ddir,theTime),self.tello.get_frame_read().frame)
+            frameRet = frame_read.frame
+
+            if args.save_session:
+                cv2.imwrite("{}/tellocap{}.jpg".format(ddir,imgCount),frame_read.frame)
+            
             frame = np.rot90(frame)
-            frame = pygame.surfarray.make_surface(frame)
-            self.screen.blit(frame, (0, 0))
-
-            bat = self.battery()
-            batCol = (255, 0, 0)
-
-            if bat >= 50:
-                batCol = (0,255,0)
-            elif bat > 15 and bat < 50:
-                batCol = (255,255,0)
-            elif bat <= 15:
-                batCol = (255,0,0)
-
-            self.DrawText("Battery: {}".format(bat),60,batCol,(32,32))
-            self.DrawText("Classifier",100,(255,255,0),(dimensions[0]/4,dimensions[1]-115))
-
-            pygame.display.update()
+            imgCount+=1
 
             time.sleep(1 / FPS)
 
+            # Press T to take off
+            if cv2.waitKey(20) & 0xFF == ord('t'):
+                if not args.debug:
+                    print("Taking Off")
+                    self.tello.takeoff()
+                    self.tello.get_battery()
+                self.send_rc_control = True
+
+            # Press L to land
+            if cv2.waitKey(20) & 0xFF == ord('l'):
+                if not args.debug:
+                    print("Landing")
+                    self.tello.land()
+                self.send_rc_control = False
+
+            gray  = cv2.cvtColor(frameRet, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=2)
+
+            # Target size
+            tSize = faceSizes[tDistance]
+
+            # Gets the width & height of the frame
+            width = 960
+            height = 720
+
+            # These are our center dimensions
+            cWidth = int(width/2)
+            cHeight = int(height/2)
+
+            noFaces = len(faces) == 0
+
+            # if we've given rc controls & get face coords returned
+            if self.send_rc_control:
+                for (x, y, w, h) in faces:
+
+                    # 
+                    roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
+                    roi_color = frameRet[y:y+h, x:x+w]
+
+                    # setting Face Box properties
+                    fbCol = (255, 0, 0) #BGR 0-255 
+                    fbStroke = 2
+                    
+                    # end coords are the end of the bounding box x & y
+                    end_cord_x = x + w
+                    end_cord_y = y + h
+                    end_size = w*2
+
+                    # these are our target coordinates
+                    targ_cord_x = int((end_cord_x + x)/2)
+                    targ_cord_y = int((end_cord_y + y)/2) + UDOffset
+
+                    # This calculates the vector from your face to the center of the screen
+                    vTrue = np.array((cWidth,cHeight,tSize))
+                    vTarget = np.array((targ_cord_x,targ_cord_y,end_size))
+                    vDistance = vTrue-vTarget
+
+                    # Safety Zone X
+                    szX = 100
+
+                    # Safety Zone Y
+                    szY = 55
+
+                    if not args.debug:
+                        # for turning
+                        if vDistance[0] < -szX:
+                            self.yaw_velocity = S
+                            # self.left_right_velocity = S2
+                        elif vDistance[0] > szX:
+                            self.yaw_velocity = -S
+                            # self.left_right_velocity = -S2
+                        else:
+                            self.yaw_velocity = 0
+                        
+                        
+                        # for up & down
+                        if vDistance[1] > szY:
+                            self.up_down_velocity = S
+                        elif vDistance[1] < -szY:
+                            self.up_down_velocity = -S
+                        else:
+                            self.up_down_velocity = 0
+
+                        # for forward back
+                        if vDistance[2] > 0:
+                            self.for_back_velocity = S
+                        elif vDistance[2] < 0:
+                            self.for_back_velocity = -S
+                        else:
+                            self.for_back_velocity = 0
+
+                    # Draw the face bounding box
+                    cv2.rectangle(frameRet, (x, y), (end_cord_x, end_cord_y), fbCol, fbStroke)
+
+                    # Draw the target as a circle
+                    cv2.circle(frameRet, (targ_cord_x, targ_cord_y), 10, (0,255,0), 2)
+
+                    # Draw the safety zone
+                    cv2.rectangle(frameRet, (targ_cord_x - szX, targ_cord_y - szY), (targ_cord_x + szX, targ_cord_y + szY), (0,255,0), fbStroke)
+
+                    # Draw the estimated drone vector position in relation to face bounding box
+                    cv2.putText(frameRet,str(vDistance),(0,64),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),2)
+
+                # if there are no faces detected, don't do anything
+                if noFaces:
+                    self.yaw_velocity = 0
+                    self.up_down_velocity = 0
+                    self.for_back_velocity = 0
+                    print("NO TARGET")
+                
+            # Draw the center of screen circle, this is what the drone tries to match with the target coords
+            cv2.circle(frameRet, (cWidth, cHeight), 10, (0,0,255), 2)
+
+            # Draw the distance choosen
+            cv2.putText(frameRet,str(tDistance),(32,664),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),2)
+
+
+            # Display the resulting frame
+            cv2.imshow(f'Tello Tracking...',frameRet)
+
+            # Quit the software
+            if cv2.waitKey(20) & 0xFF == ord('q'):
+                should_stop = True
+                break
+
+        # On exit, print the battery
+        self.tello.get_battery()
+
+        # When everything done, release the capture
+        cv2.destroyAllWindows()
+        
         # Call it always before finishing. I deallocate resources.
         self.tello.end()
 
     def battery(self):
-        return int(self.tello.get_battery()[:2])
-
-    def DrawText(self, txt, fSize, Color, Loc):
-        myfont = pygame.font.SysFont('Impact', fSize)
-        textsurface = myfont.render(txt, False, Color)
-        textRect = textsurface.get_rect()
-        textRect.center = (dimensions[0]/4, dimensions[1]/2)
-        self.screen.blit(textsurface,Loc)
-
-    def keydown(self, key):
-        """ Update velocities based on key pressed
-        Arguments:
-            key: pygame key
-        """
-        if key == pygame.K_UP:  # set forward velocity
-            self.for_back_velocity = S
-        elif key == pygame.K_DOWN:  # set backward velocity
-            self.for_back_velocity = -S
-        elif key == pygame.K_LEFT:  # set left velocity
-            self.left_right_velocity = -S
-        elif key == pygame.K_RIGHT:  # set right velocity
-            self.left_right_velocity = S
-        elif key == pygame.K_w:  # set up velocity
-            self.up_down_velocity = S
-        elif key == pygame.K_s:  # set down velocity
-            self.up_down_velocity = -S
-        elif key == pygame.K_a:  # set yaw clockwise velocity
-            self.yaw_velocity = -S
-        elif key == pygame.K_d:  # set yaw counter clockwise velocity
-            self.yaw_velocity = S
-
-    def keyup(self, key):
-        """ Update velocities based on key released
-        Arguments:
-            key: pygame key
-        """
-        if key == pygame.K_UP or key == pygame.K_DOWN:  # set zero forward/backward velocity
-            self.for_back_velocity = 0
-        elif key == pygame.K_LEFT or key == pygame.K_RIGHT:  # set zero left/right velocity
-            self.left_right_velocity = 0
-        elif key == pygame.K_w or key == pygame.K_s:  # set zero up/down velocity
-            self.up_down_velocity = 0
-        elif key == pygame.K_a or key == pygame.K_d:  # set zero yaw velocity
-            self.yaw_velocity = 0
-        elif key == pygame.K_t:  # takeoff
-            self.tello.takeoff()
-            self.send_rc_control = True
-        elif key == pygame.K_l:  # land
-            self.tello.land()
-            self.send_rc_control = False
+        return self.tello.get_battery()[:2]
 
     def update(self):
         """ Update routine. Send velocities to Tello."""
